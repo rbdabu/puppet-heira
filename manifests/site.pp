d00ac0d980a0cbf1078a7ecc9f13dceb2da8c22b
Raymond B Dabu <rbdabu@cambridge.org>
2017-06-01 11:28:35 +0800

    Added manifest directory

---
+++ manifests/site.pp
@@ -0,0 +1,29 @@
+node 'raymond-puppetmaster.buildandrelease.org' {
+	class { '::ntp':
+ 	servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
+	}
+}
+
+node 'raymond-puppetslave.buildandrelease.org' {
+        #class { '::php': }
+	#class { '::mysql::server': }
+#	user { 'rbdabu':
+#		ensure => 'present',
+#		#password => sha1('password'),
+#		password => pw_hash('password', 'SHA-512', 'mysalta'),
+#	}
+	class { 'profile::release_accounts':}
+	#class { 'profile::apache':} can also be used.
+	#include profile::apache
+	include ::role::webserver
+}
+
+node 'raymond-wordpress-1.buildandrelease.org' {
+	include role::amp
+	include profile::wordpress
+}
+
+node 'raymond-wordpress-2.buildandrelease.org' {
+        include role::amp
+        include profile::wordpress
+}
